b4ffab7cb4e1535665e8b6dca6c4551e33b9d42b
bas1c <68533770+bas1c1@users.noreply.github.com>
2024-01-11 20:33:27 +0300

    Update update.lua

--- luas/update.lua
+++ luas/update.lua
@@ -1,10 +1,10 @@
---obj = mainScene():getObject(0)
---rb = obj:get_rigidbody2D()
+obj = mainSc:getObject(0)
+rb = obj:get_rigidbody2D()
 
---if getKey('X') then
---	rb.gravity = 9.81
---elseif getKey('C') then
---	rb.gravity = -9.81
---end
+if getKey('X') then
+	rb.gravity = 9.81
+elseif getKey('C') then
+	rb.gravity = -9.81
+end
 
 mainSc:renderScene()
